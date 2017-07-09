bc55a3baadcf7b92e34fe33bb49a753e2304c476
Oliver Hofkens <oliver@novemberfive.co>
2017-07-09 15:40:37 +0200

    Finished application for now.

--- Application/server.R
+++ Application/server.R
@@ -23,10 +23,28 @@ shinyServer(function(input, output) {
         geom_ribbon(aes(x = time_period, ymin = min_value, ymax  = max_value, group = NA, color = NA), data = stats, fill = "Gray", color = NA, alpha = 0.3) +
         geom_line(aes(x = time_period, y = mean_value, group = NA), data = stats, color = "Black", linetype = 2) +
         geom_line(aes(x = time_period, y = value, group = ref_area, color = ref_area), data = dataset) +
-        scale_y_continuous(expand = c(0, 0), limits=c(0,150)) +
+        scale_y_continuous(expand = c(0, 0), limits=c(0,150), breaks = seq(0, 150, 25)) +
         labs(x = "Year", y = "Monthly Cost (€)", color = "Country") 
     
     g
+  })
+  
+  output$hoverInfo <- renderPrint({
+      x = input$plotHover$x
+      
+      stats <- subset(internetPrices, breakdown == input$speed) %>%
+          group_by(time_period) %>%
+          summarise(min_value = min(value),
+                    max_value = max(value), 
+                    mean_value = mean(value))
+      
+      year = unique(stats$time_period)[x]
+      current = subset(stats, time_period == year)
+      
+      cat("Year:", as.character(year), "\n")
+      cat("Highest price:", current$max_value, "\n")
+      cat("Lowest price:", current$min_value, "\n")
+      cat("Mean price:", current$mean_value, "\n")
   })
   
 })
--- Application/ui.R
+++ Application/ui.R
@@ -18,7 +18,20 @@ shinyUI(fluidPage(
     ),
     
     mainPanel(
-       plotOutput("pricePlot")
+       plotOutput("pricePlot", hover = hoverOpts(
+           id = "plotHover"
+       )),
+       verbatimTextOutput("hoverInfo"),
+       
+       h4("Summary"),
+       p("This interactive visualization shows the evolution of the monthly price of an internet connection across Europe."),
+       p("On the left, you can choose the speed of the connection, as well as the countries for which to show the data. 
+         The plot contains the price trend for the selected parameters, as well as general statistics across all countries for the selected connection speed."),
+       p("The gray ribbon in the plot show the maximum and minimum monthly price, and the dashed line represents the mean. The exact values for these statistics
+         will be shown in the box below when hovering over the plot."),
+       h4("Source"),
+       p("Data for this application was taken from the European Union Open Data Portal: "),
+       a("Link", href="https://data.europa.eu/euodp/en/data/dataset/nu6TLMS46TeSDUKnHAkw")
     )
   )
 ))
